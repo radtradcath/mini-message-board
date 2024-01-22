e01d90b7cb7768531e5f61eb09b8104e6aea8b27
radtradcath <vinicius.b.bonafini@gmail.com>
2024-01-22 11:11:04 -0300

    Create button to access '/new' route

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,13 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express", messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', {})
+})
+
+router.post('/new', function (req, res, next) {
+  messages.push({text: req.body.text, user: req.body.user, added: new Date()});
+  res.redirect('/')
+})
+
 module.exports = router;
